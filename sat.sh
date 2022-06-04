3567f88c9381fbc22fa86fd338232b1e4ec51f95
nikcaryo <ncaryotakis@gmail.com>
2022-06-04 12:21:48 -0700

    good to go

--- sat.sh
+++ sat.sh
@@ -1,4 +1,10 @@
-./reddit_full.sh
-./short_jokes.sh
+python3 scripts/train_local.py train config/ShortJokes/config.json -s ShortJokesInt --include-package my_library
 
-python3 scripts/train_local.py transfer config/SARC/config.json -s SarcAttempt --transfer-model ColbertShortInt/best.th --include-package my_library
+python3 scripts/train_local.py transfer config/SARC/config.json -s ColbertFine --transfer-model ColbertShortInt/best.th --include-package my_library
+python3 scripts/train_local.py transfer config/SARC/config.json -s PunsFine --transfer-model PunsInt/best.th --include-package my_library
+python3 scripts/train_local.py transfer config/SARC/config.json -s ShortJokesFine --transfer-model ShortJokesInt/best.th --include-package my_library
+
+python3 scripts/train_local.py transfer config/SARC/config.json -s EMOFine --transfer-model models/IntermediateEMO.th --include-package my_library
+python3 scripts/train_local.py transfer config/SARC/config.json -s IMDBFine --transfer-model models/IntermediateIMDB.th --include-package my_library
+python3 scripts/train_local.py transfer config/SARC/config.json -s SNTFine --transfer-model models/IntermediateSNT.th --include-package my_library
+
